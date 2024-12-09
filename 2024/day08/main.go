3917d030b05081b531e0d51bc99a199b01b5ce24
Abhinand Jha <jha.abhinand1@gmail.com>
2024-12-09 21:29:36 +0530

    Day 9

--- 2024/day08/main.go
+++ 2024/day08/main.go
@@ -3,102 +3,27 @@ package main
 import (
 	_ "embed"
 	"fmt"
-	"strings"
+	"strconv"
 )
 
 //go:embed input.txt
 var input string
 
-type Point struct {
-	x, y int
-}
-
-
-// Find (dx,dy) between two pairs
-// Calc. the left and right most points and compare with bounds
-
-func parseInput(input string) (map[rune][]Point, int, int) {
-	candidates := make(map[rune][]Point)
-	tokens := strings.Split(input, "\n")
-	n := len(tokens)
-	m := 0
-	for i, t := range tokens {
-		m = len(t)
-		for j, c := range t {
-			if c != '.' {
-				candidates[c] = append(candidates[c], Point{i,j})
-			}
-		}
+func parseInput(input string) []int {
+	expanded := make([]int, 0)
+	for i := 0; i < len(input); i++ {
+		n, _ := strconv.Atoi(string(input[i]))
+		fmt.Println(n)
 	}
-	return candidates, n, m
+	return expanded
 }
 
-
-func getDistance(p1, p2 Point) Point {
-	return Point{p2.x - p1.x, p2.y - p1.y}
+func solvePart1(input string) int {
+	expanded := parseInput(input)
+	return 0
 }
-
-func isOutside(p Point, n, m int) bool {
-	return p.x < 0 || p.x >= n || p.y < 0 || p.y >= m
-}
-
-
-func updateAntidoes(p []Point, a map[Point]struct{}, n, m int) {
-	for i := 0; i < len(p) - 1; i++ {
-		for j := i + 1; j < len(p); j++ {
-			pd := getDistance(p[i], p[j])
-			p1 := Point{p[i].x - pd.x, p[i].y - pd.y}
-			p2 := Point{p[j].x + pd.x, p[j].y + pd.y}
-			if !isOutside(p1, n, m) {
-				a[p1] = struct{}{}
-			}
-			if !isOutside(p2, n, m) {
-				a[p2] = struct{}{}
-			}
-		}
-	}
-}
-
-func updateAntidoes2(p []Point, a map[Point]struct{}, n, m int) {
-	for i := 0; i < len(p) - 1; i++ {
-		for j := i + 1; j < len(p); j++ {
-			pd := getDistance(p[i], p[j])
-			// How much can we go to the side of p1
-			newP := p[i]
-			for !isOutside(newP,n, m) {
-				a[newP] = struct{}{}
-				newP.x -= pd.x
-				newP.y -= pd.y
-			}
-			newP = p[j]
-			for !isOutside(newP,n, m) {
-				a[newP] = struct{}{}
-				newP.x += pd.x
-				newP.y += pd.y
-			}
-		}
-	}
-}
-
-func solveParts(input string, part2 bool) int {
-	candidates, n, m:= parseInput(input)
-	// Go through each pair and process
-	antidotes := make(map[Point]struct{})
-	for _, points := range candidates {
-		updateAntidoes(points, antidotes, n, m)
-		if part2 {
-			updateAntidoes2(points, antidotes, n, m)
-		}
-	}
-	return len(antidotes)
-}
-
-// Part 2 - just add max num of points
-// possible on both sides
 
 func main() {
-	ans1 := solveParts(input, false)
+	ans1 := solvePart1(input)
 	fmt.Println(ans1)
-	ans2 := solveParts(input, true)
-	fmt.Println(ans2)
 }
---
+++ 2024/day09/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+)
+
+//go:embed input.txt
+var input string
+
+func parseInput(input string) []int {
+	expanded := make([]int, 0)
+	count := 0
+	for i := 0; i < len(input); i++ {
+		n, _ := strconv.Atoi(string(input[i]))
+		r := count
+		if i % 2 != 0{
+			r = -1
+			count--
+		}
+		for k := 0; k < n; k++ {
+			expanded = append(expanded, r)
+		}
+		count++
+	}
+	return expanded
+}
+
+func checksum(arr []int) int {
+	ans := 0
+	for i, v := range arr {
+		if v != -1 {
+			ans += i * v
+		}
+	}
+	return ans
+}
+
+func doCompression(expanded []int) {
+	j, i := -1, -1 
+	for idx, v := range expanded {
+		if v != -1 {
+			j = idx
+		} 
+		if i == -1 && v == -1 {
+			i = idx
+		}
+	}
+	for i < j {
+		// swap
+		expanded[i], expanded[j] = expanded[j], expanded[i]
+		j--
+		i++
+		for j >= 0 && expanded[j] == -1 {
+			j--
+		}
+		for i < len(expanded) && expanded[i] != -1 {
+			i++
+		}
+	}
+}
+
+func tryFillFile(arr []int, fs, f, j int) {
+	for i := 0; i < len(arr); i++ {
+		if i >= j {
+			return 	
+		}
+		if arr[i] == -1 {
+			if i + fs < len(arr) {
+				fill := true
+				for k := 0; k < fs; k++ {
+					if arr[i+k] != -1 {
+						fill = false
+					}
+				}
+				if fill {
+					for k := 0; k < fs; k++ {
+						arr[i+k], arr[j+k] = arr[j+k], arr[i+k]
+					}
+					return
+				}
+			}
+		}
+	}
+}
+
+
+func doCompressionFile(arr []int) {
+	j, i := -1, -1 
+	for idx, v := range arr {
+		if v != -1 {
+			j = idx
+		} 
+		if i == -1 && v == -1 {
+			i = idx
+		}
+	}
+	for j > 0 {
+		if i > j {
+			break
+		}
+		for j >= 0 && arr[j] == -1 {
+			j--
+		}
+		// Get file size
+		f := arr[j]
+		fs := 0
+		for j >= 0 && arr[j] == f {
+			fs++
+			j--
+		}
+		// Now,
+		tryFillFile(arr, fs, f, j+1)
+	}
+}
+
+func solveParts(input string, part2 bool) int {
+	expanded := parseInput(input)
+	if !part2 {
+		doCompression(expanded)
+	} else
+	{
+		doCompressionFile(expanded)
+	}
+	return checksum(expanded)
+}
+
+func main() {
+	// ans1 := solveParts(input, false)
+	// fmt.Println(ans1)
+	ans2 := solveParts(input, true)
+	fmt.Println(ans2)
+}
