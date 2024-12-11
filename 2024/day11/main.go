d60012bf58de7822bd616d3a47fab62e34430885
Abhinand Jha <jha.abhinand1@gmail.com>
2024-12-11 18:30:22 +0530

    Day 11p2

--- 2024/day11/main.go
+++ 2024/day11/main.go
@@ -10,6 +10,11 @@ import (
 
 //go:embed input.txt
 var input string
+const numIter = 75
+
+type State struct {
+	num, iter int
+}
 
 func parseInput(input string) []int {
 	s := strings.Split(input, " ")
@@ -25,6 +30,7 @@ func parseInput(input string) []int {
 // 4048 (1 -> 4) : 4 blinks
 // 40 48 -> 4 0 4 8 -> 4 * 2024 1 4 * 2024 8 * 2024
 
+// Store distinct {n, iter} : blinks map as a cache
 func doUpdate(arr []int) []int {
 	// Follow rules and return new arr
 	res := make([]int, 0)
@@ -47,16 +53,58 @@ func doUpdate(arr []int) []int {
 }
 
 
+func doSingleUpdate(cState State, cache map[State]int) int {
+	if cState.iter >= numIter {
+		return 0
+	}
+	if _, ok := cache[cState]; ok {
+		return cache[cState]
+	}
+	num := cState.num
+	if num == 0 {
+		nState := State{1, cState.iter + 1}
+		cache[cState] = doSingleUpdate(nState, cache)
+		return cache[cState]
+	}
+	
+	numDigits := int(math.Floor(math.Log10(float64(num)))) + 1
+	if numDigits % 2 != 0 {
+		nState := State{num * 2024, cState.iter + 1}
+		cache[cState] = doSingleUpdate(nState, cache)
+		return cache[cState]
+	}
+	n := int(math.Pow10(numDigits / 2))
+	l := int(math.Floor(float64(num / n)))
+	r := num % n
+	nState1 := State{l, cState.iter + 1}
+	nState2 := State{r, cState.iter + 1}
+	cache[cState] = 1 + doSingleUpdate(nState1, cache) + doSingleUpdate(nState2, cache)
+	return cache[cState]
+}
+
+
+
 func solvePart1(input string) int {
 	candidates := parseInput(input)
 	var ans []int = candidates
-	for i := 0; i < 75; i++ {
+	for i := 0; i < numIter; i++ {
 		ans = doUpdate(ans)
 	}
 	return len(ans)
 }
 
+func solvePart2(input string) int {
+	candidates := parseInput(input)
+	cache := make(map[State]int)
+	ans := 0
+	for i := 0; i < len(candidates); i++ {
+		s := State{candidates[i], 0}
+		ans += (1 + doSingleUpdate(s, cache))
+	}
+	return ans
+}
+
 func main() {
-	ans1 := solvePart1(input)
+	ans1 := solvePart2(input)
 	fmt.Println(ans1)
 }
